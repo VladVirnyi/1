1d49db092a101c236110903e54089190d2dfd406
VladVirnyi <vladvwv15@gmail.com>
2024-09-04 18:56:58 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+hello = "Hello World"
+name = "Влад"
+surname = "Вірний"
+age = 16
+print(hello)
+print(type(hello))
+print(name)
+print(type(name))
+print(surname)
+print(type(surname))
+print(age)
+print(type(age))
